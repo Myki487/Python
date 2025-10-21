c1507e986940a73e32dc4333ca87368008414f6d
Myki487 <154702077+Myki487@users.noreply.github.com>
2025-10-21 23:09:31 +0300

    PR3(three tasks)

--- PR3/task3.py
+++ PR3/task3.py
@@ -1,11 +1,11 @@
 def replaceLetters(word):
     if len(word) < 2:
         return word
-    return word.replace('b', 'c')
+    return word.replace('b', 'c').replace('B', 'C')
 
 while True:
     print("\n--- Завдання 3 ---")
-    word = str(input("Введіть слово для заміни літер b та c (мін. 2 символи): "))
+    word = str(input("Введіть слово для заміни літер b на c (мін. 2 символи): "))
     if len(word) >= 2:
         resultWord = replaceLetters(word)
         print(f"Початкове слово: {word}")
@@ -14,5 +14,5 @@ while True:
         print("Помилка: Вам потрібно ввести рядок щонайменше з 2 символів.")
 
     answer = input("Бажаєте спробувати ще раз? (так/ні): ").lower()
-    if answer != "так":
+    if answer not in ["так", "т", "yes", "y"]:
         break
